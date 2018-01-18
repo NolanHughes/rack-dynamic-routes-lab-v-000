9d8a33125ea56c1882b0b08f812fa07c726b9f02
Nolan Hughes <nhughes987@gmail.com>
2018-01-18 18:08:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
 
      if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      binding.pry
+
       if find_item(item_name)
         resp.write find_item(item_name)
       else
