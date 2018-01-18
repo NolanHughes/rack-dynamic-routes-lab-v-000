c7e066d79f0708ee1b975d38ae26d1cda6e6a811
Nolan Hughes <nhughes987@gmail.com>
2018-01-18 18:06:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,7 @@ class Application
 
      if req.path.match(/items/)
       item_name = req.path.split("/items/").last
+      binding.pry
       resp.write find_item(item_name)
     else
       resp.write "Route not found"
