0239c81aaf37b3d68833c52fa7f430ddc18307b1
Nolan Hughes <nhughes987@gmail.com>
2018-01-18 17:32:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,10 @@ class Application
     resp = Rack::Response.new
     req = Rack::Response.new(env)
 
-    
+    if req.path == "/item"
+      resp.write "You requested an item"
+    else
+      resp.write "Route not found"
+      resp.status = 404
   end
 end
